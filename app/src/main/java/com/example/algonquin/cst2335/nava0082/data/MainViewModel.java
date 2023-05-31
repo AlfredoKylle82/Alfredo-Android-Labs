20475fec66dfd7655ba64005dc750dd953812e95
alfredokylle <alfredokylle82@gmail.com>
2023-05-31 17:36:09 -0400

    fifth commit: Finished week 2

--- app/src/main/java/com/example/algonquin/cst2335/nava0082/data/MainViewModel.java
+++ app/src/main/java/com/example/algonquin/cst2335/nava0082/data/MainViewModel.java
@@ -6,9 +6,17 @@ import androidx.lifecycle.ViewModel;
 public class MainViewModel extends ViewModel {
 
     public MutableLiveData<String> editString = new MutableLiveData<>();
-
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
     public void onChanged(String s){
 
+    }
+
+    public void setIsSelected(Boolean value) {
+        isSelected.postValue(value);
+    }
+
+    public MutableLiveData<Boolean> getIsSelected() {
+        return isSelected;
     }
 
 }
--- app/src/main/java/com/example/algonquin/cst2335/nava0082/ui/MainActivity.java
+++ app/src/main/java/com/example/algonquin/cst2335/nava0082/ui/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.algonquin.cst2335.nava0082.R;
 import com.example.algonquin.cst2335.nava0082.databinding.ActivityMainBinding;
@@ -16,21 +19,49 @@ import com.example.algonquin.cst2335.nava0082.data.MainViewModel;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding variableBinding;
     private MainViewModel model;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
+
 
         model = new ViewModelProvider(this).get(MainViewModel.class);
 
+        CompoundButton.OnCheckedChangeListener onCheckedChangeListener = (button, isChecked) -> {
+            model.setIsSelected(isChecked);
+        };
 
-        variableBinding.mybutton.setOnClickListener(click ->
-        {
-            model.editString.observe(this, s->{
-                variableBinding.textview.setText("your edit text has: "+s);
-            });
+        model.getIsSelected().observe(this, selected->{
+            variableBinding.checkBox.setOnCheckedChangeListener(null);
+            variableBinding.radioButton.setOnCheckedChangeListener(null);
+            variableBinding.switchButton.setOnCheckedChangeListener(null);
+
+
+            variableBinding.checkBox.setChecked(selected);
+            variableBinding.radioButton.setChecked(selected);
+            variableBinding.switchButton.setChecked(selected);
+
+            variableBinding.checkBox.setOnCheckedChangeListener(onCheckedChangeListener);
+            variableBinding.radioButton.setOnCheckedChangeListener(onCheckedChangeListener);
+            variableBinding.switchButton.setOnCheckedChangeListener(onCheckedChangeListener);
         });
+
+        variableBinding.checkBox.setOnCheckedChangeListener(onCheckedChangeListener);
+        variableBinding.radioButton.setOnCheckedChangeListener(onCheckedChangeListener);
+        variableBinding.switchButton.setOnCheckedChangeListener(onCheckedChangeListener);
+
+        variableBinding.myimagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int width = view.getWidth();
+                int height = view.getHeight();
+                String message = "The width = " + width + " and height = " + height;
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        setContentView(variableBinding.getRoot());
     }
 }
